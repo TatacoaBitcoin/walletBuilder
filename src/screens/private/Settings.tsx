f852671a43cde377ea61bf26be02643ea18cda4c
Daniel Trujillo <danielfetrujillo@gmail.com>
2025-08-28 15:38:58 -0500

    moves btn to settings page

--- src/screens/private/Settings.tsx
+++ src/screens/private/Settings.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import { Button } from '@react-navigation/elements';
+import { useTranslation } from 'react-i18next';
 
 import { ScreenTemplate } from '../../atoms';
+import { Routes } from '../../navigation/types';
+import { useNavigation } from '@react-navigation/native';
 
 const Settings = () => {
+  const navigation = useNavigation();
+  const { t } = useTranslation();
+
   return (
     <ScreenTemplate>
       <Text>Settings</Text>
+      <Button onPress={() => navigation.navigate(Routes.Language)}>
+        {t('Home.LanguageBtn')}
+      </Button>
     </ScreenTemplate>
   );
 };
