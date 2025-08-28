2d2c83b8c4aea3d6a3eb65968f528dc2e8c0803f
Daniel Trujillo <danielfetrujillo@gmail.com>
2025-08-28 15:23:09 -0500

    creates language selection modal

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Button } from '@react-navigation/elements';
+import { useTranslation } from 'react-i18next';
 
 import { ScreenTemplate } from '../../atoms';
 import { Routes } from '../../navigation/types';
@@ -8,11 +9,13 @@ import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
   const navigation = useNavigation();
+  const { t } = useTranslation();
+
   return (
     <ScreenTemplate>
       <Text>Home</Text>
       <Button onPress={() => navigation.navigate(Routes.Language)}>
-        Change Language
+        {t('Home.LanguageBtn')}
       </Button>
     </ScreenTemplate>
   );
---
+++ src/styles/spacing.ts
@@ -0,0 +1,30 @@
+const BASE = 4;
+
+export const padding = {
+  xs: BASE,
+  sm: BASE * 2,
+  md: BASE * 4,
+  lg: BASE * 6,
+  xl: BASE * 8,
+  xxl: BASE * 10,
+};
+
+export const margin = {
+  xs: BASE,
+  sm: BASE * 2,
+  md: BASE * 4,
+  lg: BASE * 6,
+  xl: BASE * 8,
+  xxl: BASE * 10,
+};
+
+export const fontSize = {
+  xs: BASE,
+  sm: BASE * 2,
+  smd: BASE * 3,
+  md: BASE * 4,
+  lg: BASE * 5,
+  xl: BASE * 6,
+  xxl: BASE * 7,
+  xxxl: BASE * 8,
+};
