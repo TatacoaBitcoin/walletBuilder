3e47dbccfbaa3b9308fa602e9942c7bc36ae4580
Daniel Trujillo <danielfetrujillo@gmail.com>
2025-09-04 13:49:58 -0500

    implements qr code scanning

--- src/screens/private/Scanner.tsx
+++ src/screens/private/Scanner.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import {
+  Camera,
+  Code,
+  useCameraDevice,
+  useCodeScanner,
+} from 'react-native-vision-camera';
+
+import { useIsForeground } from '../../hooks/useIsForeground';
 
 const Scanner = () => {
-  return <></>;
+  // 1. Use a simple default back camera
+  const device = useCameraDevice('back');
+
+  // 2. Only activate Camera when the app is focused and this screen is currently opened
+  const isFocused = useIsFocused();
+  const isForeground = useIsForeground();
+  const isActive = isFocused && isForeground;
+
+  // 3. (Optional) enable a torch setting
+  const [torch, setTorch] = useState(false);
+
+  // 4. On code scanned, print to console
+  const onCodeScanned = useCallback((codes: Code[]) => {
+    console.log(`Scanned ${codes.length} codes:`, codes);
+    const value = codes[0]?.value;
+    if (value == null) return;
+  }, []);
+
+  // 5. Initialize the Code Scanner to scan QR codes and Barcodes
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: onCodeScanned,
+  });
+
+  return (
+    <View style={styles.container}>
+      {device != null && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={isActive}
+          codeScanner={codeScanner}
+          torch={torch ? 'on' : 'off'}
+          enableZoomGesture={true}
+        />
+      )}
+    </View>
+  );
 };
 
 export { Scanner };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+});
