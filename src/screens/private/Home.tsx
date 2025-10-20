13b2f8e417784f38b3626c23834d9986eaab1010
Daniel Trujillo <danielfetrujillo@gmail.com>
2025-10-20 11:36:59 -0500

    adds utils file

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -9,15 +9,21 @@ import { Routes } from '../../navigation/types';
 import { TEST } from '@env';
 import Logo from '../../../assets/images/btc.svg';
 import { copyToClipboard, getFromClipboard } from '../../utils/clipboard';
+import { getStoredString, storeString } from '../../utils/storage';
 
 const Home = () => {
   const navigation = useNavigation();
   const { t } = useTranslation();
   console.log('var', TEST);
 
-  const test = async () => {
+  const getClipboard = async () => {
     const clipboard = await getFromClipboard();
     console.log(clipboard);
+  };
+
+  const getString = async () => {
+    const response = await getStoredString('box');
+    console.log(response);
   };
 
   return (
@@ -32,7 +38,11 @@ const Home = () => {
         <Button onPress={() => copyToClipboard('Test')}>
           Copy to clipboard
         </Button>
-        <Button onPress={test}>Get from clipboard</Button>
+        <Button onPress={getClipboard}>Get from clipboard</Button>
+        <Button onPress={() => storeString('box', 'test2!')}>
+          Store to storage
+        </Button>
+        <Button onPress={getString}>Get from storage</Button>
       </View>
     </ScreenTemplate>
   );
